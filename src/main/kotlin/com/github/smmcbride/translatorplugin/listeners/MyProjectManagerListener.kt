f62591d58d8f4f893115790d6e282633470e07f3
GitHub Action <action@github.com>
2023-02-17 05:21:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/smmcbride/translatorplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/smmcbride/translatorplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.smmcbride.translatorplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.smmcbride.translatorplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/smmcbride/translatorplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/smmcbride/translatorplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.smmcbride.translatorplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.smmcbride.translatorplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
